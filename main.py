a25b3abf279427538740d8423b26ccac4ad4c454
George W. <00_sling_shears@icloud.com>
2026-02-07 18:20:58 -0500

    basic media player work done

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # TODO: Find a way to circumvent the bloody YT "bot protection"
+# this bot protection only flags static IPs - figure out PO tokens later
 
 import ytp
 import dependencies
+import playlist
 
 def troubleshooter():
-    print("\n=====Troubleshooting Menu=====")
+    print("\n===== Troubleshooting =====")
     print("1. YouTube requires sign-in")
     print("2. Set up JS runtime")
     print("3. Set up FFmpeg")
-    print("4. Main menu\n")
+    print("\n===== Storage =====")
+    print("4. Set up HTTP fileserver...")
+    print("5. Main menu\n")
 
     while True:
         try:
@@ -22,6 +26,8 @@ def troubleshooter():
         elif (user_input == 3):
             dependencies.install_something("ffmpeg")
         elif (user_input == 4):
+            print("Sorry not yet implemented.")
+        elif (user_input == 5):
             main_menu()
             return 0
         else:
@@ -30,10 +36,12 @@ def troubleshooter():
 def main_menu():
     print("\n===== MediaInator =====")
     print("Select an option: ")
-    print("1. Search for a song to download")
-    print("2. Enter a video URL of the song to download")
-    print("3. Troubleshoot")
-    print("4. Exit program\n")
+    print("1. Browse music library")
+    print("2. Search for a song to download")
+    print("3. Enter a video URL of the song to download")
+    print("4. Settings")
+    print("5. Pause audio")
+    print("6. Exit program\n")
 
 main_menu()
 while True:
@@ -42,15 +50,20 @@ while True:
     except ValueError:
         print("Invalid option")
     if (user_input == 1):
+        playlist.start()
+        main_menu()
+    if (user_input == 2):
         # thing
         print("Sorry not yet implemented.")
-    elif (user_input == 2):
+    elif (user_input == 3):
         print("Enter the video ID of the song: ")
         user_url = input()
         print(ytp.download_mp3("https://youtube.com/watch?v=" + user_url))
-    elif (user_input == 3):
-        troubleshooter()
     elif (user_input == 4):
+        troubleshooter()
+    elif (user_input == 5):
+        playlist.pause()
+    elif (user_input == 6):
         break
     else:
         continue
---
+++ playlist.py
@@ -0,0 +1,64 @@
+import os
+import vlc
+# TODO: switch away from VLC somehow
+
+pathway = "./songs"
+current_song = ""
+final_list = []
+
+class song:
+    def __init__(self, song, name):
+        self.song = song
+        self.name = name
+
+def start():
+    set_up_objects()
+    print("Select a song: ")
+    for i in range(rin(final_list)):
+        print(f"({i+1}) {final_list[i]["name"]}")
+    while True:
+        try:
+            global current_song
+            user_input = int(input("Select your song... "))
+            current_song = vlc.MediaPlayer(final_list[user_input - 1]["song_path"])
+            current_song.play()
+            return
+        except ValueError:
+            print("Invalid option")
+        except IndexError:
+            print("Invalid option")
+
+def get_path(string):
+    global pathway
+    pathway = string
+
+def set_up_objects():
+    global final_list
+    song_list = os.listdir(pathway)
+    song_list.remove("names.csv")
+    for i in range(rin(song_list)):
+        dict_item = {
+            "song_path": f"{pathway}/{song_list[i]}",
+            "name": song_list[i][0:rin(song_list[i]) - 4]
+        }
+        final_list.append(dict_item)
+    
+    file = open(f"{pathway}/names.csv", "r")
+    file_list = []
+    for line in file:
+        file_list.append(line.strip().split(","))
+    file.close()
+    
+    # yes, this is definitely shitty, but will have to do
+    for i in range(rin(final_list)):
+        for j in range(rin(file_list)):
+            if (final_list[i]["song_path"] == file_list[j][1]):
+                final_list[i]["name"] = file_list[j][0]
+
+def pause():
+    global current_song
+    current_song.stop()
+
+# lol
+def rin(thing):
+    return len(thing)
